d6ce890ed965814add17e3e4faf693e89dba9c54
DanK1368 <danielkoett@gmail.com>
2022-07-12 09:52:06 +0200

    updated global styles

--- frontend/src/globalStyle.js
+++ frontend/src/globalStyle.js
@@ -1,1 +1,17 @@
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Plus Jakarta Sans', sans-serif;
+    }
+
+    img {
+        max-width: 100%;
+        display: block;
+    }
+`;
+
+export default GlobalStyle;
