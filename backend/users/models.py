05800b172a9b7a703a2eafbe0b408995b4a20b26
S.Dharnia <s.dharnia@yahoo.com>
2022-07-12 14:51:29 +0200

    user added

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    REQUIRED_FIELDS = ['username']
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField("password", max_length=128)
+    email = models.EmailField(unique=True)
+    #buyer_profile = models.ForeignKey(Registrationprofile, on_delete=models.SET_NULL, null=True)
+    #seller_profile = models.ForeignKey(Sellerprofile, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.username
+
+
