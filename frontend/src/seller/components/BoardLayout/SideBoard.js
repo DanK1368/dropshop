cbcaecab46c6e5fe127c75ea697f58431674f14f
DanK1368 <danielkoett@gmail.com>
2022-07-13 14:32:03 +0200

    updated layout

--- frontend/src/seller/components/BoardLayout/SideBoard.js
+++ frontend/src/seller/components/BoardLayout/SideBoard.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { 
-    StyledSideBar,
-    StyledSideHeader,
-    StyledSideBody,
-    StyledSideMenu,
+import {
+  StyledSideBar,
+  StyledSideHeader,
+  StyledSideBody,
+  StyledSideMenu,
 } from "../../styles/SideBoard";
 
-
-
 const SideBoard = () => {
-    return (
-        <>
-            <StyledSideBar>
-                <StyledSideHeader>
-                    <h1>DropShop</h1>
-                </StyledSideHeader>
-                <StyledSideBody>
-                    <StyledSideMenu>
-                        <button>Inventory Column</button>
-                        <button>On Sale Column</button>
-                        <button>10% Column</button>
-                        <button>25% Column</button>
-                        <button>Create New Column</button>
-                    </StyledSideMenu>
-                </StyledSideBody>
-            </StyledSideBar>
-        </>
-    );
+  return (
+    <>
+      <StyledSideBar>
+        <StyledSideHeader>
+          <h1>DropShop</h1>
+        </StyledSideHeader>
+        <StyledSideBody>
+          <StyledSideMenu>
+            <button>Inventory Column</button>
+            <button>On Sale Column</button>
+            <button>10% Column</button>
+            <button>25% Column</button>
+            <button>Create New Column</button>
+          </StyledSideMenu>
+        </StyledSideBody>
+      </StyledSideBar>
+    </>
+  );
 };
 
 export default SideBoard;
