b30114c5767a62e94adf392778e6ebe34f350f04
Melissa Guimaraes <mg.melissaguimaraes@gmail.com>
2022-07-13 20:10:45 +0200

    bottom component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import BottomPicture from "./buyer/components/BottomPicture";
 import ItemForSale from "./buyer/components/ItemForSale";
 import SellerPage from "./seller/pages/SellerPage";
 
 function App() {
   return (
     <div>
-      <ItemForSale />
+      <BottomPicture/>
+      {/* <ItemForSale /> */}
       {/**
       <Routes>
         <Route path="/" element={<SellerPage />} />
