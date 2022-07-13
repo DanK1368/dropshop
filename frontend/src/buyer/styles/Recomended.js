b6cc21477e799577a49834d1dd9d15fe1b7892b9
Melissa Guimaraes <mg.melissaguimaraes@gmail.com>
2022-07-13 21:21:14 +0200

    recomended items created

---
+++ frontend/src/buyer/styles/Recomended.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+
+export const OuterDiv = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    font-weight:bold;
+    padding-bottom:15px;`
+
+export const Main = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    //border: 2px solid green;
+    `
+
+export const Column = styled.div`
+    margin: 10px 10px;
+    //border: 2px solid black;`
+
+export const Picture = styled.div`
+    height:180px;
+    width:180px;`
+
+
+export const Model = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    font-weight:bold;
+    font-size:12px;
+    //border:2px solid pink;
+    
+    button{
+        height:40px;
+        width:140px;
+        color:white;
+        background: #d87d4a;
+        border:none;
+        //border-radius:2px;
+        font-size:12px;
+
+    }`
