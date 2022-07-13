e5a59450e6ca8c6d0fcdcff832be9129c48b2ea0
DanK1368 <danielkoett@gmail.com>
2022-07-13 14:27:45 +0200

    updated how functionality is to be implemented for drag and drop

--- frontend/src/seller/components/BoardLayout/DisplayBoard/Columns/ForSaleColumn.js
+++ frontend/src/seller/components/BoardLayout/DisplayBoard/Columns/ForSaleColumn.js
@@ -1,12 +1,28 @@
-import React from "react";
-import {Page, Columns} from "../../../../styles/SellerColumns";
+import React, { useState } from "react";
+import { Columns, Grid } from "../../../../styles/SellerColumns";
+import { useDrop } from "react-dnd";
 
 const ForSaleColumn = () => {
-    return(
-        <Page>
-        <Columns><h4>For Sale</h4></Columns>
-        </Page>
-    )
-}
+  const [board, setBoard] = useState([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "productItem",
+    drop: item => addItemToList(item.id),
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addItemToList = itemId => {
+    setBoard([]);
+  };
+
+  return (
+    <Columns ref={drop}>
+      <h4>For Sale</h4>
+      <Grid></Grid>
+    </Columns>
+  );
+};
 
 export default ForSaleColumn;
