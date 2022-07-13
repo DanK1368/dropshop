52862c1df60025396620d533a4b5fb1e22486345
DanK1368 <danielkoett@gmail.com>
2022-07-13 14:31:54 +0200

    delete item functionality added

--- frontend/src/seller/components/BoardLayout/DisplayBoard/Items/SingleItem.js
+++ frontend/src/seller/components/BoardLayout/DisplayBoard/Items/SingleItem.js
@@ -1,15 +1,45 @@
 import styled from "styled-components";
 import { StyledCard } from "../../../../styles/SingleItem";
+import { useDrag } from "react-dnd";
+import { useState } from "react";
+import { TiDelete } from "react-icons/ti";
+import DeleteItem from "../../../../components/BoardLayout/DisplayBoard/Items/DeleteItem";
 
-const SingleItem = () => {
+const SingleItem = ({ id, name, price, stock, showDeleteButton }) => {
+  const [showWarning, setShowWarning] = useState(false);
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "productItem",
+    item: {
+      id: id,
+    },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
   return (
-    <StyledCard>
-      <h2>Headphones PX5000</h2>
-      <div>
-        <p>Price: $300</p>
-        <p>Stock: 5 </p>
-      </div>
-    </StyledCard>
+    <>
+      <StyledCard ref={drag}>
+        {showDeleteButton && (
+          <button onClick={() => setShowWarning(!showWarning)}>
+            <TiDelete size={25} color="#EA5555" />
+          </button>
+        )}
+        <h2>{name}</h2>
+        <div>
+          <p>Price: $ {price}</p>
+          <p>Stock: {stock} </p>
+        </div>
+      </StyledCard>
+      {showWarning && (
+        <DeleteItem
+          showWarning={showWarning}
+          setShowWarning={setShowWarning}
+          productId={id}
+        />
+      )}
+    </>
   );
 };
 export default SingleItem;
