30a80ced2742fdff593dc6ce95d442cf67f2c58f
DanK1368 <danielkoett@gmail.com>
2022-07-13 14:30:53 +0200

    adjusted layout

--- frontend/src/seller/styles/SellerColumns.js
+++ frontend/src/seller/styles/SellerColumns.js
@@ -1,17 +1,37 @@
 import styled from "styled-components";
 
-export const Page = styled.div`
-    display:flex;
-    flex-direction:row;
-    width:30%; 
-`
 export const Columns = styled.div`
-    display:grid;
-    grid-template-columns: repeat(1, 1fr);
-    //grid:;
-    //gap:;
-    color:white;
-    background-color:#000112;
-    padding:5px 10px;
+  max-width: 300px;
+  height: 80%;
+  color: white;
+  padding: 5px 10px;
+  text-align: center;
+  background-color: rgba(43, 44, 55, 0.5);
+  margin-left: 1rem;
+  border-radius: 10px;
 
-    `
+  h4 {
+    color: #a8a4ff;
+    font-size: 24px;
+  }
+`;
+
+export const Grid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: repeat(11, 1fr);
+  gap: 1rem;
+  place-items: center;
+  overflow: scroll;
+  padding-top: 0.5rem;
+`;
+// export const Columns = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+//   //grid:;
+//   //gap:;
+//   color: white;
+//   background-color: green;
+//   padding: 5px 10px;
+// `;
