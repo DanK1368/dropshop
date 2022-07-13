78d4c718df13844d34b9a7c555b2f96a9d6b5d3b
DanK1368 <danielkoett@gmail.com>
2022-07-13 14:26:20 +0200

    route added for the seller page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import Header from "./seller/components/BoardLayout/Header";
+import { Routes, Route } from "react-router-dom";
 import SellerPage from "./seller/pages/SellerPage";
 
-
 function App() {
-  return <div>
-    <SellerPage/>
-  </div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<SellerPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
