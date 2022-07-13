1b26991e521b32fad8087230b863807c13f01d4a
Julian Bräuninger <j.br34@hotmail.com>
2022-07-13 18:30:17 +0000

    Update frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,11 @@ import SellerPage from "./seller/pages/SellerPage";
 function App() {
   return (
     <div>
-      <BottomPicture/>
-      {/* <ItemForSale /> */}
-      {/**
+      
       <Routes>
         <Route path="/" element={<SellerPage />} />
       </Routes>
-       */}
+      
     </div>
   );
 }
