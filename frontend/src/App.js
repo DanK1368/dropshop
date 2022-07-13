8b2163e2a55df078d0d9a1ad7ac83dc4c0ea07dc
Julian Bräuninger <j.br34@hotmail.com>
2022-07-13 14:03:17 +0200

    Pictures are too low

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./seller/components/BoardLayout/Header";
 import SellerPage from "./seller/pages/SellerPage";
+import ShopLinks from "./buyer/components/ShopLinks";
 
 
 function App() {
   return <div>
-    <SellerPage/>
+    <ShopLinks />
+    {/*<SellerPage/>*/}
   </div>;
 }
 
---
+++ frontend/src/buyer/styles/ShopLinks.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const StyledAllLinks = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 80%;
+
+`
+
+export const StyledHeadphones = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    height: 0;
+    padding-bottom: 20%;
+    background-color: #f1f1f1;
+    border-radius: 10px;
+    align-items: center;
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+
+    button {
+        border: none;
+    }
+`
+
+export const StyledSpeakers = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    height: 0;
+    padding-bottom: 20%;
+    background-color: #f1f1f1;
+    border-radius: 10px;
+    align-items: center;
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+
+    button {
+        border: none;
+    }
+`
+
+export const StyledEarphones = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    height: 0;
+    padding-bottom: 20%;
+    background-color: #f1f1f1;
+    border-radius: 10px;
+    align-items: center;
+
+    img {
+        width: 100px;
+        height: 100px;
+    }
+
+    button {
+        border: none;
+    }
+`
