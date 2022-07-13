0d43ab712869ea2c4a619095b8c9d90cc1c4f7ff
Alexander B <alphadelta395@outlook.com>
2022-07-13 15:27:28 +0200

    finished categories

--- backend/category/models.py
+++ backend/category/models.py
@@ -7,4 +7,4 @@ class CategoryModel(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"id: {self.name} | name: {self.id}"
+        return f"Id: {self.id} | Name: {self.name}"
