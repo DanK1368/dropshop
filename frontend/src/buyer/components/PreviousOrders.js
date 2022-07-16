e3363fc5740056f948fc2d8c6e95a9eba2b3d411
DanK1368 <danielkoett@gmail.com>
2022-07-16 17:49:40 +0200

    created new files for styled-components and imported it to the specific component

--- frontend/src/buyer/components/PreviousOrders.js
+++ frontend/src/buyer/components/PreviousOrders.js
@@ -1,62 +1,9 @@
-import styled from "styled-components";
+import {
+  StyledCartContainer,
+  StyledFlexContainer,
+  StyledProductContainer,
+} from "../styles/PreviousOrders";
 import image1 from "../../assets/product-xx59-headphones/desktop/image-product.jpg";
-
-export const StyledCartContainer = styled.div`
-  min-width: 250px;
-  width: 100%;
-  border-radius: 10px;
-  background-color: white;
-  background-color: white;
-  padding: 1.5rem 2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-  flex: 1;
-  align-self: start;
-
-  img {
-    width: 70px;
-    height: 70px;
-    object-fit: cover;
-    border-radius: 10px;
-  }
-`;
-
-export const StyledFlexContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const StyledProductContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 1.5rem;
-
-  div:nth-child(1) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  p {
-    font-size: 14px;
-  }
-
-  p:nth-child(1) {
-    font-weight: 500;
-    margin: 0;
-    margin-bottom: 0.5rem;
-  }
-  p:nth-child(2) {
-    font-weight: 500;
-    color: gray;
-    margin: 0;
-  }
-`;
 
 const PreviousOrders = () => {
   return (
---
+++ frontend/src/buyer/styles/Profile.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+export const StyledOuterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  padding: 2rem;
+  flex: 3;
+  gap: 3rem;
+
+  h1 {
+    font-weight: 500;
+  }
+
+  h2 {
+    color: #d87d4a;
+    font-size: 16px;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+`;
+
+export const StyledInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  label {
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  input {
+    padding: 1rem;
+    border-radius: 10px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+
+    &:focus {
+      outline: none;
+      border: 1px solid #d87d4a;
+    }
+  }
+`;
+
+export const StyledInputGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+`;
+
+export const StyledBtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const StyledSaveBtn = styled.button`
+  padding: 1rem 2rem;
+  border: none;
+  background-color: #d87d4a;
+  font-size: 14px;
+  letter-spacing: 1px;
+  font-weight: 300;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background: #fbaf85;
+  }
+`;
+
+export const StyledDeleteBtn = styled.button`
+  padding: 1rem 1.5rem;
+  border: none;
+  background-color: #ea5555;
+  font-size: 14px;
+  letter-spacing: 1px;
+  font-weight: 300;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background: #ff9898;
+  }
+`;
+
+export const StyledRightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  align-self: start;
+`;
