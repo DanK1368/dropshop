bc330c92b7c86a891ac2796bdd096fc42e503568
DanK1368 <danielkoett@gmail.com>
2022-07-18 09:16:55 +0200

    Registration functionality added using a temporary local endpoint

--- frontend/src/buyer/pages/Registration.js
+++ frontend/src/buyer/pages/Registration.js
@@ -1,9 +1,31 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { StyledContainer, StyledForm } from "../styles/Login";
+import { registerUser } from "../../redux/apiCalls";
 
 const Registration = () => {
+  const navigate = useNavigate();
+  const { pending, status } = useSelector(store => store.user);
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
+
+  const handleRegistration = e => {
+    e.preventDefault();
+    registerUser(email, dispatch);
+  };
+
+  // navigate to validation page if registration is successfull
+  useEffect(() => {
+    if (status === 201) {
+      navigate("/validation");
+    } else {
+      return;
+    }
+  }, [status]);
+
   return (
-    <StyledContainer>
+    <StyledContainer onSubmit={handleRegistration}>
       <h1>
         <span>Register</span> <br /> your new account
       </h1>
@@ -14,6 +36,8 @@ const Registration = () => {
             name="email"
             id="email"
             placeholder="E-Mail Address"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </label>
 
