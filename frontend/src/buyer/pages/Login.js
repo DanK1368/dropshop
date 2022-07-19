5fb05029732415abcf27115bcd6e8252db9f622e
DanK1368 <danielkoett@gmail.com>
2022-07-19 08:35:03 +0200

    update user information functionality implemented, including a spinner to show while processes are loading

--- frontend/src/buyer/pages/Login.js
+++ frontend/src/buyer/pages/Login.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { StyledContainer, StyledForm } from "../styles/Login";
 import { loginUser } from "../../redux/apiCalls";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import spinner2 from "../../assets/spinner2.gif";
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { pending } = useSelector(store => store.user);
   const [loginValues, setLoginValues] = useState({
     username: "",
     password: "",
@@ -22,40 +24,44 @@ const Login = () => {
       <h1>
         <span>Login</span> <br /> To Your Account
       </h1>
-      <StyledForm onSubmit={handleLogin}>
-        <label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-            value={loginValues.username}
-            onChange={e =>
-              setLoginValues(prevState => {
-                return { ...prevState, [e.target.name]: e.target.value };
-              })
-            }
-          />
-        </label>
-        <label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-            value={loginValues.password}
-            onChange={e =>
-              setLoginValues(prevState => {
-                return { ...prevState, [e.target.name]: e.target.value };
-              })
-            }
-          />
-        </label>
-        <button>SUBMIT</button>
-        <p>
-          Don't have an account? Sign up <Link to="/registration">here</Link>
-        </p>
-      </StyledForm>
+      {pending ? (
+        <img src={spinner2} alt="" />
+      ) : (
+        <StyledForm onSubmit={handleLogin}>
+          <label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              value={loginValues.username}
+              onChange={e =>
+                setLoginValues(prevState => {
+                  return { ...prevState, [e.target.name]: e.target.value };
+                })
+              }
+            />
+          </label>
+          <label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              value={loginValues.password}
+              onChange={e =>
+                setLoginValues(prevState => {
+                  return { ...prevState, [e.target.name]: e.target.value };
+                })
+              }
+            />
+          </label>
+          <button>SUBMIT</button>
+          <p>
+            Don't have an account? Sign up <Link to="/registration">here</Link>
+          </p>
+        </StyledForm>
+      )}
     </StyledContainer>
   );
 };
