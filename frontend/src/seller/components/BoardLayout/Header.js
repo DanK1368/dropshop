d8a367f5986069bb56d785e18e9c92b2fa0cebf5
Julian Bräuninger <j.br34@hotmail.com>
2022-07-19 11:03:52 +0200

    sideboard deleted

--- frontend/src/seller/components/BoardLayout/Header.js
+++ frontend/src/seller/components/BoardLayout/Header.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import { StyledHeader, AddButton } from "../../styles/Header";
 import AddItem from "../../components/BoardLayout/DisplayBoard/Items/AddItem";
+import AddColumn from "../../components/BoardLayout/DisplayBoard/Items/AddColumn";
 
 const Header = () => {
   const [showAddNewItem, setShowAddNewItem] = useState(false);
+  const [showAddNewColumn, setShowAddNewColumn] = useState(false);
 
   const handleShowAddNewItem = () => setShowAddNewItem(!showAddNewItem);
+  const handleShowAddNewColumn = () => setShowAddNewColumn(!showAddNewColumn);
 
   return (
     <>
       <StyledHeader>
-        <h3>Platform Launch</h3>
-        <AddButton onClick={handleShowAddNewItem}>
-          {showAddNewItem ? "Cancel" : "+Add New Item"}
-        </AddButton>
+        <div>
+          <h2>dropshop</h2>
+          <p>Dashboard</p>
+        </div>
+        <div>
+          <AddButton onClick={handleShowAddNewItem}>
+            {showAddNewItem ? "Cancel" : "+Add New Item"}
+          </AddButton>
+          <AddButton onClick={handleShowAddNewColumn}>
+            {showAddNewColumn ? "Cancel" : "+Add New Column"}
+          </AddButton>
+        </div>
       </StyledHeader>
       {showAddNewItem && <AddItem />}
+      {showAddNewColumn && <AddColumn />}
     </>
   );
 };
--- frontend/src/seller/styles/Header.js
+++ frontend/src/seller/styles/Header.js
@@ -10,13 +10,25 @@ export const StyledHeader = styled.div`
   background-color: #2b2c37;
   border-bottom: 0.5px solid #828fa3;
 
-  h3 {
+  div {
+    display: flex;
+    flex-direction: row;
+  }
+
+  h2 {
     color: white;
+  }
+
+  p {
+    color: white;
+    padding: 12px 20px;
   }
 `;
 
+
 export const AddButton = styled.button`
   padding: 10px 15px;
+  margin: 10px;
   color: white;
   border: none;
   border-radius: 90px;
